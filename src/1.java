5af6c577d988975414577a1d9b107bc2ed8c17d7
harcourtjluis <harcourtjluis@gmail.com>
2025-03-05 19:53:42 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class JavaCode {
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 1; i <= 10; i++) {
+      list.add(i);
+    }
+    System.out.println(list);
+  }
+}
