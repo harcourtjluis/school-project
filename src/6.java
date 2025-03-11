4e2f434250aba969ec8faa2861e43524e1f9e091
harcourtjluis <harcourtjluis@gmail.com>
2025-03-11 00:10:59 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class RandomJavaCode {
+    public static void main(String[] args) {
+        // Generate a random integer between 1 and 10
+        int randomNumber = (int)(Math.random()*10)+1;
+
+        // Print the result
+        System.out.println("The random number is: " + randomNumber);
+    }
+}
