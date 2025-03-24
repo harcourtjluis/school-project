e76824ed39e8b01d2bc66bb075a8bca270ffe9ab
harcourtjluis <harcourtjluis@gmail.com>
2025-03-24 11:10:01 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,11 @@
+public class SchoolProject {
+    // Example method to demonstrate using loops
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Looping through numbers");
+            i++;
+        }
+    }
+}
