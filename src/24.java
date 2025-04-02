4a3cf42be3ae95c32aa6366d16b98a5ee2943d5a
harcourtjluis <harcourtjluis@gmail.com>
2025-04-02 12:46:15 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,12 @@
+public class SchoolProject {
+    private String projectName;
+
+    public SchoolProject(String projectName) {
+        this.projectName = projectName;
+    }
+
+    // Method to display project information
+    public void displayProjectInfo() {
+        System.out.println("Project Name: " + projectName);
+    }
+}
