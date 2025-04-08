98f9395fe76ce27d1c15261b57692e1c82b823b9
harcourtjluis <harcourtjluis@gmail.com>
2025-04-08 13:04:40 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example of basic Java code
+        System.out.println("Hello, World!");
+        
+        // Example of accessing arrays and variables
+        int[] array = {1, 2, 3};
+        double sum = 0.0;
+        for (int i : array) {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
