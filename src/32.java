90b0fc68a98276456697a001400be79b500c5274
harcourtjluis <harcourtjluis@gmail.com>
2025-04-11 11:16:42 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,10 @@
+public class SchoolProject {
+    // Function to demonstrate string manipulation
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        
+        System.out.println("Concatenating: " + str1 + " and " + str2);
+        System.out.println(str1 + str2); // Concatenation using a combination of strings
+    }
+}
