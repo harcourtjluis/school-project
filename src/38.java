217d7f60fae5ffd278138356ce68a916ed85ac22
harcourtjluis <harcourtjluis@gmail.com>
2025-05-17 06:15:50 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,19 @@
+public class ProjectCodeGenerator {
+    public static void main(String[] args) {
+        // Example of initializing variables
+        int num1 = 5;
+        double pi = Math.PI;
+
+        // Example of performing arithmetic operations
+        int sum = num1 + 2;
+        double product = pi * 3;
+
+        // Example of accessing and using class methods
+        String str = "Hello, World!";
+        System.out.println(str.length());  // Length of the string
+
+        // Example of creating objects and invoking their methods
+        Student student = new Student("John", 25);
+        student.displayInfo();
+    }
+}
